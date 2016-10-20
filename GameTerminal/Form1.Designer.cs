306071839c739ccab201ea725e7714f9efeafb69
MaxDelavalle <mdela015@plattsburgh.edu>
2016-10-19 21:21:59 -0400

    Gameterminal update- more buttons + user/pass

--- GameTerminal/Form1.Designer.cs
+++ GameTerminal/Form1.Designer.cs
@@ -31,15 +31,23 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Salmon;
-            this.button1.Location = new System.Drawing.Point(14, 22);
+            this.button1.Location = new System.Drawing.Point(35, 133);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(428, 44);
+            this.button1.Size = new System.Drawing.Size(169, 44);
             this.button1.TabIndex = 0;
             this.button1.Text = "Rock Paper Scissors";
             this.button1.UseVisualStyleBackColor = false;
@@ -49,10 +57,10 @@
             // 
             this.button2.BackColor = System.Drawing.Color.Salmon;
             this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(14, 93);
+            this.button2.Location = new System.Drawing.Point(278, 133);
             this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(428, 48);
+            this.button2.Size = new System.Drawing.Size(168, 44);
             this.button2.TabIndex = 1;
             this.button2.Text = "SpaceInvaders";
             this.button2.UseVisualStyleBackColor = false;
@@ -61,21 +69,104 @@
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Salmon;
-            this.button3.Location = new System.Drawing.Point(14, 175);
+            this.button3.Location = new System.Drawing.Point(510, 133);
             this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(428, 44);
+            this.button3.Size = new System.Drawing.Size(168, 44);
             this.button3.TabIndex = 2;
             this.button3.Text = "Snake";
             this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Salmon;
+            this.button4.Location = new System.Drawing.Point(35, 239);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 44);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Snake";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Salmon;
+            this.button5.Location = new System.Drawing.Point(278, 239);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(168, 44);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Snake";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Salmon;
+            this.button6.Location = new System.Drawing.Point(510, 239);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(168, 44);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Snake";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(319, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(510, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(390, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(578, 16);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Yellow;
+            this.button7.Location = new System.Drawing.Point(35, 16);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(117, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Create Account";
+            this.button7.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(456, 248);
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(714, 307);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -85,6 +176,7 @@
             this.Name = "Form1";
             this.Text = "GAME TERMINAL";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -93,6 +185,14 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button7;
     }
 }
 
