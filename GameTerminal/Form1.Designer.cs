611b073d398fa7758ede270072b58f380378e282
MaxDelavalle <mdela015@plattsburgh.edu>
2016-12-05 12:51:02 -0500

    added create account form

--- GameTerminal/Form1.Designer.cs
+++ GameTerminal/Form1.Designer.cs
@@ -120,6 +120,7 @@
             this.button7.TabIndex = 10;
             this.button7.Text = "Create Account";
             this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button8
             // 
--- GameTerminal/Form1.cs
+++ GameTerminal/Form1.cs
@@ -1,6 +1,6 @@
 ﻿using CalceranosInvaders;
+using GameTerminal;
 using HangmanD_7;
-using Pingpong;
 using Snake;
 using System;
 using System.Collections.Generic;
@@ -84,7 +84,12 @@ namespace WindowsFormsApplication2
 
         private void button5_Click(object sender, EventArgs e)
         {
-            pongForm popup = new pongForm();
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            accountForm popup = new accountForm();
             DialogResult dialogresult = popup.ShowDialog();
             if (dialogresult == DialogResult.Cancel)
             {
